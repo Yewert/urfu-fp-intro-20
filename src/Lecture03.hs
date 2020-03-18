cf431ed9b47fb11335a5ec3f7d32db55564a1193
Yewert <blazer.andrew@gmail.com>
2020-03-19 00:18:39 +0500

    wrong case?

--- src/Lecture03.hs
+++ src/Lecture03.hs
@@ -459,7 +459,7 @@ isZero = "\\x. x (\\f. false) true"
   Что она возвращает? Чему равен pred pred 1?
 -}
 fib :: String
-fib = "Y (\\f.\\n. ifelse (isZero n) (1) (plus (f (pred n)) (f (pred (pred n)))))"
+fib = "Y (\\f.\\n. ifelse (iszero n) (1) (plus (f (pred n)) (f (pred (pred n)))))"
 
 -- </Задачи для самостоятельного решения>
 
